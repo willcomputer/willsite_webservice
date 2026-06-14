dd0008d819f132ea9e7c7952ea5f590a94ebf1a1
William Bucher <wjbucher3@gmail.com>
2026-06-14 17:17:07 -0400

    removed a lot

--- src/app.py
+++ src/app.py
@@ -21,17 +21,6 @@ def pi_health():
     response = jsonify(temperature=temp, cpuLoadPercentage=cpu_load, percentUsedMemory=percent_used_memory)
     return add_cors_headers(response)
 
-@app.route('/auth/blog', methods=['GET'])
-def blog_auth():
-    input = request.args.get('password')
-    response = make_response()
-
-    if input == blog_password:
-        response = jsonify(result=True)
-    else:
-        response = jsonify(result=False)
-
-    return add_cors_headers(response)
 
 @app.route('/get-blogs', methods=['GET'])
 def fetch_blogs():
@@ -44,30 +33,6 @@ def fetch_blogs():
 
     return add_cors_headers(response)
 
-@app.route('/get-development-blogs', methods=['GET'])
-def fetch_development_blogs():
-    response = make_response()
-
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, 'static', 'development-blogs.json')
-    data = json.load(open(json_url))
-    response = jsonify(data)
-
-    return add_cors_headers(response)
-
-@app.route('/get-private-blogs', methods=['GET'])
-def fetch_private_blogs():
-    response = make_response()
-
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, 'static', 'private-blogs.json')
-    data = json.load(open(json_url))
-    response = jsonify(data)
-    
-    print("#############################")
-    print("private blogs accessed!")
-    print("#############################")
-    return add_cors_headers(response)
 
 @app.route('/add-email', methods=['GET'])
 def add_email_address():
@@ -87,87 +52,9 @@ def add_email_address():
         response = jsonify(message=False)
         return add_cors_headers(response)
 
-@app.route('/get-bird-list', methods=['GET'])
-def fetch_bird_list():
-    response = make_response()
-
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, 'static', 'birds.json')
-    data = json.load(open(json_url))
-    response = jsonify(data)
-
-    return add_cors_headers(response)
-
-
-@app.route('/get-bird-photo-list', methods=['GET'])
-def fetch_bird_photo_id_list():
-
-    input = request.args.get('bird-name')
-    response = make_response()
-
-    response = make_response()
-
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, 'static', 'birds-photo-ids.json')
-    data = json.load(open(json_url))
-    response = jsonify(photos=data[input])
-
-    return add_cors_headers(response)
-
-
-@app.route('/get-bird-image', methods=['GET'])
-def fetch_bird_image():
-    input = request.args.get('image-name')  
-    return send_file('static/photos/birds/' + input, mimetype='image/gif')
-
-
-@app.route('/get-tamagotchi', methods=['GET'])
-def fetch_tamagotchi():
-    response = make_response()
-
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, 'tamagotchi.json')
-    data = json.load(open(json_url))
-    response = jsonify(data)
-
-    return add_cors_headers(response)
 
 
 
-@app.route('/update-tamagotchi', methods=['POST'])
-def update_tamagotchi():
-
-    data = request.json
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, 'tamagotchi.json')
-
-    jsonFile = open(json_url, "w+")
-    jsonFile.write(json.dumps(data))
-    jsonFile.close()
-
-    response = jsonify(message=True)
-    return add_cors_headers(response)
-
-@app.route('/feed-tamagotchi', methods=['GET'])
-def feed_tamagotchi():
-    
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, 'tamagotchi.json')
-
-    jsonFile = open(json_url, "w+")
-    jsonFile['isHungry'] = False
-    jsonFile.write(json.dumps(data))
-    jsonFile.close()
-    response = jsonify(message=True)
-    return add_cors_headers(response)
-
-@app.route('/get-tamagotchi-sprite', methods=['GET'])
-def fetch_tamagotchi_sprite():
-    input = request.args.get('age')  
-    with open('static/tamagotchi.json') as f:
-        photo_dic = json.loads(f.read())
-
-    return send_file('static/photos/tamagotchi/' + input + ".png", mimetype='image/gif')
 
 
 @app.route('/get-rds-report', methods=['GET'])
@@ -176,48 +63,6 @@ def fetch_rds_report():
 
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     json_url = os.path.join(SITE_ROOT, 'static', 'rds.json')
-    data = json.load(open(json_url))
-    response = jsonify(data)
-
-    return add_cors_headers(response)
-
-
-@app.route('/get-book-cover', methods=['GET'])
-def fetch_book_cover():
-    input = request.args.get('id')  
-    with open('static/book-covers.json') as f:
-        photo_dic = json.loads(f.read())
-
-    return send_file('static/photos/book-covers/' + photo_dic[input]["file"], mimetype='image/gif')
-
-@app.route('/get-completed-books', methods=['GET'])
-def fetch_completed_books():
-    response = make_response()
-
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, 'static', 'books-finished.json')
-    data = json.load(open(json_url))
-    response = jsonify(data)
-
-    return add_cors_headers(response)
-
-@app.route('/get-current-books', methods=['GET'])
-def fetch_current_books():
-    response = make_response()
-
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, 'static', 'books-current-reading.json')
-    data = json.load(open(json_url))
-    response = jsonify(data)
-
-    return add_cors_headers(response)
-
-@app.route('/get-upcoming-books', methods=['GET'])
-def fetch_upcoming_books():
-    response = make_response()
-
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, 'static', 'books-upcoming.json')
     data = json.load(open(json_url))
     response = jsonify(data)
 
@@ -293,72 +138,6 @@ def send_update_email():
         return add_cors_headers(response)
 
 
-@app.route('/send-development-update', methods=['GET'])
-def send_dev_update_email():
-
-    real = 'thewillbucher@gmail.com'
-    alias = 'will@will.computer'
-    toaddy = alias
-    subject = 'New Development Blog Alert!'
-    body = 'A new development blog has been posted, go check it out! https://will.computer/development-blog'
-    bcc = []
-
-    with open('email-list.csv', newline='\n') as csvfile:
-        rows = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in rows:
-            bcc.append(row[0])
-
-    print("Sending to the following:")
-    print(bcc)
-
-    message = MIMEMultipart()
-    message["From"] = alias
-    message["To"] = toaddy
-    message["Subject"] = subject
-    message.attach(MIMEText(body, "plain"))
-
-    response = make_response()
-    try:
-        send_email(real, alias, toaddy, bcc, message)
-        response = jsonify(message=True)
-        return add_cors_headers(response)
-    except: 
-        response = jsonify(message=False)
-        return add_cors_headers(response)
-
-@app.route('/send-private-update', methods=['GET'])
-def send_private_update_email():
-
-    real = 'thewillbucher@gmail.com'
-    alias = 'will@will.computer'
-    toaddy = alias
-    subject = 'New Private Blog Alert!'
-    
-    body = 'A new private blog has been posted, the password right now is "'+ blog_password +'". Go check it out! https://will.computer/blog'
-    bcc = []
-
-    with open('private-email-list.csv', newline='\n') as csvfile:
-        rows = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in rows:
-            bcc.append(row[0])
-
-    print("Sending to the following:")
-    print(bcc)
-
-    message = MIMEMultipart()
-    message["From"] = alias
-    message["To"] = toaddy
-    message["Subject"] = subject
-    message.attach(MIMEText(body, "plain"))
-
-    response = make_response()
-    try:
-        send_email(real, alias, toaddy, bcc, message)
-        response = jsonify(message=True)
-        return add_cors_headers(response)
-    except: 
-        response = jsonify(message=False)
-        return add_cors_headers(response)
 
 def send_email(real_email, alias_email, toaddy, bcc, message):
     server = 'smtp.gmail.com'
