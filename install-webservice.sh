76dddc0626abd445e485ae901b201bc0c57d696f
William Bucher <wjbucher3@gmail.com>
2026-06-14 16:44:30 -0400

    updated service

--- install-webservice.sh
+++ install-webservice.sh
@@ -1,3 +1,7 @@
-
+#!/bin/bash
 
 sudo cp webservice.service /etc/systemd/system/webservice.service 
+
+sudo systemctl daemon-reload
+sudo systemctl enable webservice
+sudo systemctl start webservice
