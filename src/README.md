b69f4a8ab53e6a9fb3caf5a0c3f3eef438375161
William Bucher <wjbucher3@gmail.com>
2026-06-21 15:10:48 -0400

    refactor

--- src/README.md
+++ src/README.md
@@ -1,1 +1,20 @@
 # willsite_webservice
+
+
+Start the virtual environment 
+
+```
+source .venv/bin/activate
+```
+
+If it is the first time running the virtual environment, install packages
+
+```
+pip install flask flask_cors psutil bcrypt
+```
+
+Run the game
+
+```
+python3 main.py
+```
--- src/app.py
+++ src/app.py
@@ -1,17 +1,127 @@
 from flask import Flask, jsonify, request, make_response, send_file
 from flask_cors import CORS
 from gpiozero import CPUTemperature
-import psutil, os, json, smtplib, email, csv, configparser
-import json
+import psutil, os, json, bcrypt
 
-from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-
-blog_password='police'
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
+custom_salt = b"$2b$12$Rtp8g8Y1mE1mE1mE1mE1mu"
 
+######################################################################
+### User Authentication Endpoints
+######################################################################
+
+@app.route('/login', methods=['GET'])
+def login():
+    username = request.args.get('username')
+    password = request.args.get('password')
+
+    if (not(doesUserExist)):
+        return add_cors_headers(invalidUsernameRepsonse())
+    
+    if (not(isPasswordCorrect)):
+        return add_cors_headers(badPasswordRepsonse()) 
+    
+    return add_cors_headers(make_response(jsonify({"session": generateSessionToken(username)}), 200))
+    
+
+@app.route('/signup', methods=['GET'])
+def signup():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    
+    if (doesUserExist):
+        return add_cors_headers(takenUsernameRepsonse())
+    
+    createNewUser(username, password)
+    createNewUserInStats(username)
+    
+    return add_cors_headers(make_response(jsonify({"session": generateSessionToken(username)}), 200))
+ 
+    
+######################################################################
+### Modify stored data 
+######################################################################
+    
+def createNewUserInAccounts(username, password): 
+    path = "static/accounts.json"
+    account_map = {}
+    with open(path, "w") as f1:
+        account_map = json.loads(f.read())
+        f1.close()        
+    
+    account_map[username] = bcrypt.hashpw(password, custom_salt)
+
+    with open(path, "w") as f2:
+        json.dump(account_map, f2, indent=4)
+        f2.close()
+        
+        
+def createNewUserInStats(username):
+    path = "static/stats.json"
+    stats_map = {}
+    with open(path, "w") as f1:
+        stats_map = json.loads(f.read())
+        f1.close()        
+    
+    stats_map[username] = {"spins": 0, "points": 0}
+
+    with open(path, "w") as f2:
+        json.dump(stats_map, f2, indent=4)
+        f2.close()
+        
+        
+def generateSessionToken(username):
+    return "fake_session_token_please_fix_me"
+        
+        
+######################################################################
+### Verifications 
+######################################################################
+
+def validationSessionToken(username: str, token: str):
+    return True
+
+def isPasswordCorrect(username:str, password: str) -> bool:
+    hashed_input = bcrypt.hashpw(password, custom_salt)
+    match = False
+    with open('static/accounts.json') as f:
+        account_map = json.loads(f.read())
+        if username in account_map:
+            match = account_map[username] == hashed_input
+        f.close()
+    return match
+
+def doesUserExist(username: str) -> bool:
+    exists = False;
+    with open('static/accounts.json') as f:
+        account_map = json.loads(f.read())
+        exists = username in account_map
+        f.close()
+    return exists
+
+def getUserStats(username: str): 
+    user_stats = {}
+    with open('static/stats.json') as f:
+        stats_map = json.loads(f.read())
+        if username in stats_map: 
+            user_stats = stats_map[username]
+        f.close()
+    return user_stats
+     
+######################################################################
+### Game Logic
+######################################################################
+
+
+# create the following methods
+# 1. roll(username, sessionToken) - rolls the slot and returns the ending slot state and updated player stats
+# 2. fetchStats(username) - indepth stats for player 
+
+
+######################################################################
+### Pi Health Check
+######################################################################
 
 @app.route('/pi-health', methods=['GET'])
 def pi_health():
@@ -21,153 +131,29 @@ def pi_health():
     response = jsonify(temperature=temp, cpuLoadPercentage=cpu_load, percentUsedMemory=percent_used_memory)
     return add_cors_headers(response)
 
+######################################################################
+### Responses 
+######################################################################
 
-@app.route('/get-blogs', methods=['GET'])
-def fetch_blogs():
-    response = make_response()
+def takenUsernameRepsonse():
+    return make_response(jsonify({"message": "Username already exists"}), 404)
 
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, 'static', 'blogs.json')
-    data = json.load(open(json_url))
-    response = jsonify(data)
+def invalidUsernameRepsonse():
+    return make_response(jsonify({"message": "Username does not exist"}), 404)
 
-    return add_cors_headers(response)
-
-
-@app.route('/add-email', methods=['GET'])
-def add_email_address():
-    input = request.args.get('email')
-
-    with open('email-list.csv', 'a', newline='\n') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow([input])
-
-    response = make_response()
-    try:
-        send_confirmation(input)
-        response = jsonify(message=True)
-        return add_cors_headers(response)
-    except: 
-        response = jsonify(message=False)
-        return add_cors_headers(response)
-
-
-
-
-
-
-@app.route('/get-rds-report', methods=['GET'])
-def fetch_rds_report():
-    response = make_response()
-
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, 'static', 'rds.json')
-    data = json.load(open(json_url))
-    response = jsonify(data)
-
-    return add_cors_headers(response)
-
-
-
-@app.route('/get-image', methods=['GET'])
-def fetch_image():
-    input = request.args.get('id')  
-    with open('static/photos.json') as f:
-        
-        photo_dic = json.loads(f.read())
-        # print(photo_dic[input])
-
-    return send_file('static/photos/' + photo_dic[input]["file"], mimetype='image/gif')
-
-@app.route('/get-image-description', methods=['GET'])
-def fetch_image_description(): 
-    input = request.args.get('id')  
-    with open('static/photos.json') as f:
-        photo_dic = json.loads(f.read())
-        # print(photo_dic[input])
-
-    response = jsonify(description=photo_dic[input]["description"])
-    return add_cors_headers(response)
-
-def send_confirmation(input):
-    real = 'thewillbucher@gmail.com'
-    alias = 'will@will.computer'
-    toaddy = input
-    subject = 'Welcome to the Mailing List'
-    body = 'Confirmed! You\'ve been added to the mailing list to receive notifications when I post new blogs.'
-    bcc = []
-    message = MIMEMultipart()
-    message["From"] = alias
-    message["To"] = toaddy
-    message["Subject"] = subject
-    message.attach(MIMEText(body, "plain"))
-    send_email(real, alias, toaddy, bcc, message)
-
-@app.route('/send-update', methods=['GET'])
-def send_update_email():
-
-    real = 'thewillbucher@gmail.com'
-    alias = 'will@will.computer'
-    toaddy = alias
-    subject = 'New Blog Alert!'
-    body = 'A new blog has been posted, go check it out! https://will.computer/blog'
-    bcc = []
-
-    with open('email-list.csv', newline='\n') as csvfile:
-        rows = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in rows:
-            bcc.append(row[0])
-
-    print("Sending to the following:")
-    print(bcc)
-
-    message = MIMEMultipart()
-    message["From"] = alias
-    message["To"] = toaddy
-    message["Subject"] = subject
-    message.attach(MIMEText(body, "plain"))
-
-    response = make_response()
-    try:
-        send_email(real, alias, toaddy, bcc, message)
-        response = jsonify(message=True)
-        return add_cors_headers(response)
-    except: 
-        response = jsonify(message=False)
-        return add_cors_headers(response)
-
-
-
-def send_email(real_email, alias_email, toaddy, bcc, message):
-    server = 'smtp.gmail.com'
-    port = 587
-    pw = get_email_password()
+def badPasswordRepsonse():
+    return make_response(jsonify({"message": "Invalid Password"}), 404)
     
-    mail = smtplib.SMTP(server, port)
-
-    mail.ehlo()
-    mail.starttls()
-    mail.login(real_email, pw )
-    try:
-        mail.sendmail(alias_email, [toaddy] + bcc, message.as_string())
-        print('E-mail sent.')
-    except:
-        print('E-mail not sent.')
-
-    mail.close()
-
-def get_email_password():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    return config.get('Email', 'password')
-
 def add_cors_headers(response):
     response.headers.add("Access-Control-Allow-Origin", "*")
     response.headers.add("Access-Control-Allow-Headers", "GET, POST")
     response.headers.add("Access-Control-Allow-Methods", "Content-Type") 
     return response
 
+
+######################################################################
+### Python shit
+######################################################################
 if __name__ == '__main__':
     app.run()
 
